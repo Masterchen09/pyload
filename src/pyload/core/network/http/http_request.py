d2f9f958a45e52c9633c291ee557ed3e73de977e
Masterchen09 <13187726+Masterchen09@users.noreply.github.com>
2024-08-22 14:50:58 +0200

    Added config for HTTP request timeout.

--- src/pyload/core/network/http/http_request.py
+++ src/pyload/core/network/http/http_request.py
@@ -233,6 +233,9 @@ class HTTPRequest:
         if "timeout" in options:
             self.c.setopt(pycurl.LOW_SPEED_TIME, int(options["timeout"]))
 
+        if "timeout_speed" in options:
+            self.c.setopt(pycurl.LOW_SPEED_LIMIT, int(options["timeout_speed"]))
+
         if "ssl_verify" in options:
             self._configure_ssl(options["ssl_verify"])
 
